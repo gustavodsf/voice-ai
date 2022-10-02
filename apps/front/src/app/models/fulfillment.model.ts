f15482062fde0467daaa47217645e8d28b95f4e1
Gustavo Figueiredo <gustavodsf1@gmail.com>
2022-10-02 14:09:37 -0300

    basic structure

--- apps/front/src/app/models/fulfillment.model.ts
+++
@@ -1,9 +0,0 @@
-export class Fulfillment {
-    UTTERANCE: string | undefined;
-    FULFILLMENTS: Match[] | undefined;
-}
-
-export class Match {
-    QUESTION: string | undefined;
-    ANSWER: string | undefined;
-}
